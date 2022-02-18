62b45b14d72392545b38d6f3c14b0e6a719ce512
Jackson Tai <jacksontai21@gmail.com>
2022-02-18 22:13:04 +0800

    Modified Footer
    Changed some classes name to make it look cleaner and reduced few lines of css code

--- public/layout/footer.php
+++ public/layout/footer.php
@@ -1,23 +1,16 @@
 <footer class="layout-footer">
-
-     <div class="footer-content">
-          <h2 class="layout-footer__title">Ex-Ledge</h2>
-          <p class="layout-footer__title">______________________________</p>
-          <div class="social-media">
-               <i class="fa-brands fa-twitter" style="font-size: 2rem; margin-right: 30px;"></i>
-               <i class="fa-brands fa-facebook" style="font-size: 2rem; margin-right: 30px;"></i>
-               <i class="fa-brands fa-instagram" style="font-size: 2rem;"></i>
-          </div> 
-
-          <div class="footer-text">
-               <a class="layout-footer__text" style="left: 18em" href="#">About Us</a>
-               <p class="layout-footer__copyright">Copyright &copy;
-                    <?php echo date('Y'); ?> Ex-Ledge
-               </p>
-               <a class="layout-footer__text" style="right: 13em" href="#">Terms and Conditions</a>
-          </div>
+     <h2 class="layout-footer__title">Ex-Ledge</h2>
+     <div class="layout-footer__social-media">
+          <i class="layout-footer__social-icon fa-brands fa-twitter"></i>
+          <i class="layout-footer__social-icon fa-brands fa-facebook"></i>
+          <i class="layout-footer__social-icon fa-brands fa-instagram"></i>
      </div>
-     
-     
-</footer>
 
+     <div class="layout-footer__content">
+          <a class="layout-footer__text" href="#">About Us</a>
+          <p class="layout-footer__copyright">Copyright &copy;
+               <?php echo date('Y'); ?> Ex-Ledge
+          </p>
+          <a class="layout-footer__text" href="#">Terms and Conditions</a>
+     </div>
+</footer>
