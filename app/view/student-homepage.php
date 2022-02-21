844152c491edcad630194fa4446827c6f44b8907
jy <16880388+jyyyyylim@users.noreply.github.com>
2022-02-21 10:16:47 +0800

    init files for studentpage

---
+++ app/view/student-homepage.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include '../../public/layout/head.php' ?>
+    <link rel="stylesheet" href="../../public/css/verification.css">
+    <title>Ex-Ledge | Sign-in</title>
+</head>
+
+<body>
+
+    <?php include '../../public/layout/header.php'; ?>
+
+
+
+    <?php include '../../public/layout/footer.php'; ?>
+
+    <script src="../../public/js/script.js"></script>
+</body>
+
+</html>
