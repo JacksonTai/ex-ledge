a1e8242b9bf7be8acf08c00b60d80d528f122f8f
E Heng <3eheng2988@gmail.com>
2022-02-24 12:11:45 +0800

    Added back admin_dashboard and did a few changes.

--- app/view/admin/dashboard.php
+++ app/view/admin/dashboard.php
@@ -3,6 +3,7 @@ session_start();
 require '../../helper/redirector.php';
 $path = '../../../';
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,15 +17,55 @@ $path = '../../../';
 
     <?php include '../layout/header.php'; ?>
 
-    <div class="main-sidebar-wrapper">
+    <div class="container">
+        <div class="main-sidebar-wrapper">
 
-        <?php include '../layout/sidebar.php' ?>
+            <?php include '../layout/sidebar.php' ?>
 
-        <main class="dashboard--main main-content">
-            <h2 class="dashboard__title">Admin Dashboard</h2>
-            <p><?= $_SESSION['userId']; ?></p>
-        </main>
+            <main class="dashboard--main main-content">
+                <h2 class="dashboard__title">Admin Dashboard</h2>
+                <p><?= $_SESSION['userId']; ?></p>
+                
+                <div class="panel">
+                    <div class="panel-card-stats">
+                        <p class="panel-title">Total visits</p>
+                        <p class="panel-title-stat">12089</p>
+                    </div>
+                    <div class="panel-detail registered-users">
+                        <div class="panel-card-stats">
+                            <p class="panel-card-stat-count">3089</p>
+                            <p class="panel-card-title">Registered user</p>
+                        </div>
+                    </div>
+                    <div class="panel-detail total-users">
+                        <div class="panel-card-stats">
+                        <p class="panel-card-stat-count">7890</p>
+                        <p class="panel-card-title">Total User</p>
+                        </div>
+                    </div>
+                </div>
 
+                <div class="panel">
+                    <div class="panel-card-stats">
+                        <p class="panel-title">Total answered</p>
+                        <p class="panel-title-stat">79%</p>
+                    </div>
+                    <div class="panel-detail total-ans">
+                        <div class="panel-card-stats">
+                            <p class="panel-card-stat-count">591</p>
+                            <p class="panel-card-title">Total Answers</p>
+                        </div>
+                    </div>
+                    <div class="panel-detail total-questions">
+                        <div class="panel-card-stats">
+                            <p class="panel-card-stat-count">743</p>
+                            <p class="panel-card-title">Total Questions</p>
+                        </div>
+                    </div>
+                </div>
+            </main>
+
+        </div>
     </div>
 
     <?php include '../layout/footer.php'; ?>
