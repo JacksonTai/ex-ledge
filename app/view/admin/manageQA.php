ebb4550f29c0bc26071155b54d082c5428aaeeb4
E Heng <3eheng2988@gmail.com>
2022-02-25 21:01:04 +0800

    Followed figma's prototype, still work in progress

--- app/view/admin/manageQA.php
+++ app/view/admin/manageQA.php
@@ -23,6 +23,51 @@ $path = '../../../';
           <main class="manageQA--main main-content">
                <h2 class="manageQA__title">Manage Q&A</h2>
                <p><?= $_SESSION['userId']; ?></p>
+
+               <div class="panel">
+                    <div class="vote-container">
+                         <i class="fa-solid fa-arrow-up"></i>
+                         <p>95</p>
+                         <i class="fa-solid fa-arrow-down"></i>
+                    </div>
+
+                    <div class="question-headers">
+                         <div class="question-caption">
+                              <p class="question-title">Is Ex-Ledge the best?</p>
+                              <p class="question-age">19 mins ago</p>
+                         </div>
+                         
+                         <div class="button">
+
+                              <button class="button-answer">
+                                   <p>12 Answers</p>
+                              </button>
+
+                              <button class="button-remove">
+                                   <p>Remove</p>
+                              </button>
+                              
+
+                         </div>
+
+                    </div>
+               </div>
+
+               <div class="panel">
+                    <div class="vote-container">
+                         <i class="fa-solid fa-arrow-up"></i>
+                         <p>95</p>
+                         <i class="fa-solid fa-arrow-down"></i>
+                    </div>
+               </div>
+
+               <div class="panel">
+                    <div class="vote-container">
+                         <i class="fa-solid fa-arrow-up"></i>
+                         <p>95</p>
+                         <i class="fa-solid fa-arrow-down"></i>
+                    </div>
+               </div>
           </main>
 
      </div>
