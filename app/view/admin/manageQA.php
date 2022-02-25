5e0c59afba88c01d5a63c8ac464253b77814b968
E Heng <3eheng2988@gmail.com>
2022-02-25 21:30:36 +0800

    Only responsive not completed

--- app/view/admin/manageQA.php
+++ app/view/admin/manageQA.php
@@ -26,47 +26,116 @@ $path = '../../../';
 
                <div class="panel">
                     <div class="vote-container">
-                         <i class="fa-solid fa-arrow-up"></i>
+                         <i class="fa-solid fa-arrow-up fa-lg"></i>
                          <p>95</p>
-                         <i class="fa-solid fa-arrow-down"></i>
+                         <i class="fa-solid fa-arrow-down fa-lg"></i>
                     </div>
 
-                    <div class="question-headers">
-                         <div class="question-caption">
-                              <p class="question-title">Is Ex-Ledge the best?</p>
-                              <p class="question-age">19 mins ago</p>
-                         </div>
-                         
-                         <div class="button">
-
-                              <button class="button-answer">
-                                   <p>12 Answers</p>
-                              </button>
-
-                              <button class="button-remove">
-                                   <p>Remove</p>
-                              </button>
+                    <div class="question">
+                       <div class="question-headers">
+                              <div class="question-caption">
+                                   <p class="question-title">Is Ex-Ledge the best?</p>
+                                   <p class="question-age">19 mins ago</p>
+                              </div>
                               
+                              <div class="button">
+                                   <button class="button-answer">
+                                        <p>12 Answers</p>
+                                   </button>
 
+                                   <button class="button-remove">
+                                        <p>Remove</p>
+                                   </button>
+                              </div>
                          </div>
 
+                         <div class="question-body">
+                              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo </p>
+                         </div>
+
+                         <div class="question-footer">
+                              <img class="profile-picture" src="https://preview.redd.it/v0caqchbtn741.jpg?auto=webp&s=c5d05662a039c031f50032e22a7c77dfcf1bfddc" alt="profile-picture">
+                              <p class="posted-by">Posted by&nbsp</p>
+                              <p class="owner">Mike Wazowski</p>
+                         </div>
                     </div>
+                    
                </div>
 
                <div class="panel">
                     <div class="vote-container">
-                         <i class="fa-solid fa-arrow-up"></i>
+                         <i class="fa-solid fa-arrow-up fa-lg"></i>
                          <p>95</p>
-                         <i class="fa-solid fa-arrow-down"></i>
+                         <i class="fa-solid fa-arrow-down fa-lg"></i>
                     </div>
+
+                    <div class="question">
+                       <div class="question-headers">
+                              <div class="question-caption">
+                                   <p class="question-title">Is Ex-Ledge the best?</p>
+                                   <p class="question-age">19 mins ago</p>
+                              </div>
+                              
+                              <div class="button">
+                                   <button class="button-answer">
+                                        <p>12 Answers</p>
+                                   </button>
+
+                                   <button class="button-remove">
+                                        <p>Remove</p>
+                                   </button>
+                              </div>
+                         </div>
+
+                         <div class="question-body">
+                              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo </p>
+                         </div>
+
+                         <div class="question-footer">
+                              <img class="profile-picture" src="https://preview.redd.it/v0caqchbtn741.jpg?auto=webp&s=c5d05662a039c031f50032e22a7c77dfcf1bfddc" alt="profile-picture">
+                              <p class="posted-by">Posted by&nbsp</p>
+                              <p class="owner">Mike Wazowski</p>
+                         </div>
+                    </div>
+                    
                </div>
 
                <div class="panel">
                     <div class="vote-container">
-                         <i class="fa-solid fa-arrow-up"></i>
+                         <i class="fa-solid fa-arrow-up fa-lg"></i>
                          <p>95</p>
-                         <i class="fa-solid fa-arrow-down"></i>
+                         <i class="fa-solid fa-arrow-down fa-lg"></i>
                     </div>
+
+                    <div class="question">
+                       <div class="question-headers">
+                              <div class="question-caption">
+                                   <p class="question-title">Is Ex-Ledge the best?</p>
+                                   <p class="question-age">19 mins ago</p>
+                              </div>
+                              
+                              <div class="button">
+                                   <button class="button-answer">
+                                        <p>12 Answers</p>
+                                   </button>
+
+                                   <button class="button-remove">
+                                        <p>Remove</p>
+                                   </button>
+                              </div>
+                         </div>
+
+                         <div class="question-body">
+                              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo </p>
+                         </div>
+
+                         <div class="question-footer">
+                              <img class="profile-picture" src="https://preview.redd.it/v0caqchbtn741.jpg?auto=webp&s=c5d05662a039c031f50032e22a7c77dfcf1bfddc" alt="profile-picture">
+                              <p class="posted-by">Posted by&nbsp</p>
+                              <p class="owner">Mike Wazowski</p>
+                         </div>
+                    </div>
+                    
                </div>
           </main>
 
