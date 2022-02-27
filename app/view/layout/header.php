16f9f193de4ed296630cf3d6cb9e8b2b69d5f612
Jackson Tai <74141823+JacksonTai@users.noreply.github.com>
2022-02-27 14:58:36 +0800

    Fix user & admin sidebar (#13)
    * Fix user sidebar 
    
    - previously the sidebar can be scroll down just as the main section. 
    - now the sidebar position is fix even when the user scroll
    
    * Revert "Fix user sidebar "
    
    This reverts commit ee49c5097e7f5ccf3bc2fdd990b0422463975d76.
    
    * Fix user&admin sidebar
    
    - previously the sidebar can be scroll down just as the main section. 
    - now the sidebar position is fix even when the user scroll
    
    * Some progress for profile 
    
    
    - add some in-progress html code for profile 
    - add a 35 x 35px profile dummy image 
    - just changed the header html code a little bit
    - previously using div to wrap the link.
    - now use more semantic html code (nav, ul, li)

--- app/view/layout/header.php
+++ app/view/layout/header.php
@@ -26,26 +26,32 @@ strpos($url, 'app/view') ? $imgPath = '../../../public/img/icons/' : $imgPath = 
     </h1>
     <nav class="layout-header__nav">
         <?php if (isset($_SESSION['userId'])) : ?>
-            <div class="layout-header__btn-container">
+            <ul class="layout-header__nav-list">
+                <!-- <a class="layout-header__nav-link layout-header__nav-link--profile" href="profile.php">
+                    <img class="layout-header__profile-img" src="../../../public/img/profile.jpg" alt="Profile Image">
+                    <span class="layout-header__profile-name">Username</span>
+                </a> -->
                 <?php if ($_SESSION['userId'][0] == "A") : ?>
-                    <a class="layout-header__btn--admin" href="../admin/dashboard.php">Dashboard</a>
-                    <a class="layout-header__btn--admin" href="../admin/manageQA.php">Manage Q&A</a>
-                    <a class="layout-header__btn--admin" href="../admin/manageUser.php">Manage user</a>
-                    <a class="layout-header__btn--admin" href="../admin/userVerification.php">User Verification</a>
+                    <li><a class="layout-header__nav-link" href="../admin/dashboard.php">Dashboard</a></li>
+                    <li><a class="layout-header__nav-link" href="../admin/manageQA.php">Manage Q&A</a></li>
+                    <li><a class="layout-header__nav-link" href="../admin/manageUser.php">Manage user</a></li>
+                    <li><a class="layout-header__nav-link" href="../admin/userVerification.php">User Verification</a></li>
                 <?php elseif ($_SESSION['userId'][0] == "U") : ?>
-                    <a class="layout-header__btn--admin" href="../student/home.php">Home</a>
-                    <a class="layout-header__btn--admin" href="../student/user.php">Users</a>
-                    <a class="layout-header__btn--admin" href="../student/leaderboard.php">Leaderboard</a>
+                    <li><a class="layout-header__nav-link" href="../student/home.php">Home</a></li>
+                    <li><a class="layout-header__nav-link" href="../student/user.php">Users</a></li>
+                    <li><a class="layout-header__nav-link" href="../student/leaderboard.php">Leaderboard</a></li>
                 <?php endif; ?>
-                <a class="layout-header__btn--admin" href="../../helper/logout.php">Log Out</a>
-            </div>
+                <li><a class="layout-header__nav-link" href="../../helper/logout.php">Log Out</a></li>
+            </ul>
         <?php else : ?>
             <?php if (!strpos($url, 'signin') && !strpos($url, 'signup')) : ?>
                 <div class="layout-header__btn-container">
                     <a class="layout-header__btn layout-header__btn--signin" href="<?php echo $entryPath; ?>signin.php">
                         Sign in
                     </a>
-                    <a class="layout-header__btn layout-header__btn--signup" href="<?php echo $entryPath; ?>signup.php">Sign up</a>
+                    <a class="layout-header__btn layout-header__btn--signup" href="<?php echo $entryPath; ?>signup.php">
+                        Sign up
+                    </a>
                 </div>
             <?php endif; ?>
         <?php endif; ?>
