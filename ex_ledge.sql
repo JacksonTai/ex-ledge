2d90a60bb35a45730d375e2bbd2e8b363d967642
Jackson Tai <jacksontai21@gmail.com>
2022-03-03 11:25:20 +0800

    Fix database
    - Change length of id related field into 20 varchar
    - Change question and answer content data type to medium text
    - Change comment and message content data type to text
    - Change email field from varchar 255 to 319
      ref: https://dba.stackexchange.com/questions/37014/in-what-data-type-should-i-store-an-email-address-in-database
    - set default value for point field as 0
    - set default value for verification field in user table as 0
    - set default value for status field in user table as 1
    - set null value of nric_no field in personal detail table as yes

--- ex_ledge.sql
+++ ex_ledge.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 24, 2022 at 03:36 AM
+-- Generation Time: Mar 03, 2022 at 04:14 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -24,32 +24,307 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `answer`
+--
+
+CREATE TABLE `answer` (
+  `answer_id` varchar(20) NOT NULL,
+  `question_id` varchar(20) NOT NULL,
+  `user_id` varchar(20) NOT NULL,
+  `content` mediumtext NOT NULL,
+  `point` int(11) NOT NULL DEFAULT 0,
+  `status` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookmark_answer`
+--
+
+CREATE TABLE `bookmark_answer` (
+  `user_id` varchar(20) NOT NULL,
+  `answer_id` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookmark_question`
+--
+
+CREATE TABLE `bookmark_question` (
+  `user_id` varchar(20) NOT NULL,
+  `question_id` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+
+CREATE TABLE `comment` (
+  `comment_id` varchar(20) NOT NULL,
+  `user_id` varchar(20) NOT NULL,
+  `reply_id` varchar(20) NOT NULL,
+  `content` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message`
+--
+
+CREATE TABLE `message` (
+  `message_id` varchar(20) NOT NULL,
+  `sender_id` varchar(20) NOT NULL,
+  `receiver_id` varchar(20) NOT NULL,
+  `content` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `question`
+--
+
+CREATE TABLE `question` (
+  `question_id` varchar(20) NOT NULL,
+  `user_id` varchar(20) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `content` mediumtext NOT NULL,
+  `point` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
-  `user_id` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `full_name` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `user_id` varchar(20) NOT NULL,
+  `email` varchar(319) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `verification` tinyint(1) NOT NULL DEFAULT 0,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `point` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `email`, `full_name`, `password`) VALUES
-('A6214d5dbb1305', 'admin@gmail.com', 'admin', '$2y$10$WZkAFhFYfkHVnMPx2rFH3OI19eMHAaX8OHg5v5gx/tuLofgzH9NS2');
+INSERT INTO `user` (`user_id`, `email`, `username`, `password`, `verification`, `status`, `point`) VALUES
+('A6214d5dbb1305', 'admin@gmail.com', 'Admin', '$2y$10$WZkAFhFYfkHVnMPx2rFH3OI19eMHAaX8OHg5v5gx/tuLofgzH9NS2', 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_detail`
+--
+
+CREATE TABLE `user_detail` (
+  `user_id` varchar(20) NOT NULL,
+  `nric_no` int(12) DEFAULT NULL,
+  `bio` varchar(3000) NOT NULL,
+  `gender` varchar(5) NOT NULL,
+  `contact_no` varchar(11) NOT NULL,
+  `age` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `verification_queue`
+--
+
+CREATE TABLE `verification_queue` (
+  `nric_no` int(12) NOT NULL,
+  `user_id` varchar(20) NOT NULL,
+  `full_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vote_answer`
+--
+
+CREATE TABLE `vote_answer` (
+  `answer_id` varchar(20) NOT NULL,
+  `user_id` varchar(20) NOT NULL,
+  `vote` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vote_question`
+--
+
+CREATE TABLE `vote_question` (
+  `question_id` varchar(20) NOT NULL,
+  `user_id` varchar(20) NOT NULL,
+  `vote` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `answer`
+--
+ALTER TABLE `answer`
+  ADD PRIMARY KEY (`answer_id`),
+  ADD KEY `fk_answer__question` (`question_id`),
+  ADD KEY `fk_answer__user` (`user_id`);
+
+--
+-- Indexes for table `bookmark_answer`
+--
+ALTER TABLE `bookmark_answer`
+  ADD PRIMARY KEY (`answer_id`,`user_id`),
+  ADD KEY `fk_bookmark_answer__user` (`user_id`);
+
+--
+-- Indexes for table `bookmark_question`
+--
+ALTER TABLE `bookmark_question`
+  ADD PRIMARY KEY (`user_id`,`question_id`),
+  ADD KEY `fk_bookmark_question__question` (`question_id`);
+
+--
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`comment_id`),
+  ADD KEY `fk_comment__user` (`user_id`),
+  ADD KEY `fk_comment__answer` (`reply_id`);
+
+--
+-- Indexes for table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`message_id`),
+  ADD KEY `fk_message-sender__user` (`sender_id`),
+  ADD KEY `fk_message-receiver__user` (`receiver_id`);
+
+--
+-- Indexes for table `question`
+--
+ALTER TABLE `question`
+  ADD PRIMARY KEY (`question_id`),
+  ADD KEY `fk_question__user` (`user_id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `user_detail`
+--
+ALTER TABLE `user_detail`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `verification_queue`
+--
+ALTER TABLE `verification_queue`
+  ADD PRIMARY KEY (`nric_no`),
+  ADD KEY `fk_verification_queue__user` (`user_id`);
+
+--
+-- Indexes for table `vote_answer`
+--
+ALTER TABLE `vote_answer`
+  ADD PRIMARY KEY (`user_id`,`answer_id`),
+  ADD KEY `fk_vote_answer__answer` (`answer_id`);
+
+--
+-- Indexes for table `vote_question`
+--
+ALTER TABLE `vote_question`
+  ADD PRIMARY KEY (`user_id`,`question_id`),
+  ADD KEY `fk_vote_question__question` (`question_id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `answer`
+--
+ALTER TABLE `answer`
+  ADD CONSTRAINT `fk_answer__question` FOREIGN KEY (`question_id`) REFERENCES `question` (`question_id`),
+  ADD CONSTRAINT `fk_answer__user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `bookmark_answer`
+--
+ALTER TABLE `bookmark_answer`
+  ADD CONSTRAINT `fk_bookmark_answer__answer` FOREIGN KEY (`answer_id`) REFERENCES `answer` (`answer_id`),
+  ADD CONSTRAINT `fk_bookmark_answer__user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `bookmark_question`
+--
+ALTER TABLE `bookmark_question`
+  ADD CONSTRAINT `fk_bookmark_question__question` FOREIGN KEY (`question_id`) REFERENCES `question` (`question_id`),
+  ADD CONSTRAINT `fk_bookmark_question__user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `comment`
+--
+ALTER TABLE `comment`
+  ADD CONSTRAINT `fk_comment__answer` FOREIGN KEY (`reply_id`) REFERENCES `answer` (`answer_id`),
+  ADD CONSTRAINT `fk_comment__question` FOREIGN KEY (`reply_id`) REFERENCES `question` (`question_id`),
+  ADD CONSTRAINT `fk_comment__user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `message`
+--
+ALTER TABLE `message`
+  ADD CONSTRAINT `fk_message-receiver__user` FOREIGN KEY (`receiver_id`) REFERENCES `user` (`user_id`),
+  ADD CONSTRAINT `fk_message-sender__user` FOREIGN KEY (`sender_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `question`
+--
+ALTER TABLE `question`
+  ADD CONSTRAINT `fk_question__user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `user_detail`
+--
+ALTER TABLE `user_detail`
+  ADD CONSTRAINT `fk_user_detail__user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `verification_queue`
+--
+ALTER TABLE `verification_queue`
+  ADD CONSTRAINT `fk_verification_queue__user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `vote_answer`
+--
+ALTER TABLE `vote_answer`
+  ADD CONSTRAINT `fk_vote_answer__answer` FOREIGN KEY (`answer_id`) REFERENCES `answer` (`answer_id`),
+  ADD CONSTRAINT `fk_vote_answer__user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `vote_question`
+--
+ALTER TABLE `vote_question`
+  ADD CONSTRAINT `fk_vote_question__question` FOREIGN KEY (`question_id`) REFERENCES `question` (`question_id`),
+  ADD CONSTRAINT `fk_vote_question__user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
