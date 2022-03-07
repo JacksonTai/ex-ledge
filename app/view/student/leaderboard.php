6d885464b40aecb000f1fcc54e183df40d9f26cb
hochangyong1@gmail.com <hochangyong1@gmail.com>
2022-03-07 21:11:22 +0800

    fixed responsive design of user and leaderboard

--- app/view/student/leaderboard.php
+++ app/view/student/leaderboard.php
@@ -23,87 +23,78 @@ $path = '../../../';
 
           <main class="leaderboard--main main-content">
                <h2 class="leaderboard__title">Leaderboard</h2>
-               <div class="container">
-                    <div class="leaderboard-dashboard">
-                         
-                         <div class="leaderboard-container">
-                              
-                              <!-- top 3 -->
-                              <div class="s_box">
-                                   <div class="top2-s_half_box">
-                                        <p class="pt">1123 pt</p>
-                                        <img class="top_img" src="https://wac-cdn.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg?cdnVersion=231" alt="top img">
-                                        <h3>changyongg</h3>
-                                   </div>
-                              </div>
-
-                              <div class="s_box">
-                                   <div class="top1-s_half_box">
-                                        <p class="pt">1123 pt</p>
-                                        <img class="top_img" src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80" alt="top img">
-                                        <h3>changyongg</h3>
-                                   </div>
-                              </div>
-
-                              <div class="s_box">
-                                   <div class="top3-s_half_box">
-                                        <p class="pt">1123 pt</p>
-                                        <img class="top_img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlIy4kRyqQYE-eyQQEMvkgyyDv0IEHECKuNQ&usqp=CAU" alt="top img">
-                                        <h3>changyongg</h3>
-                                   </div>
+               <div class="leaderboard_dashboard">
+                    <div class="leaderboard_container">
+                         <div class="top_box top2_box">
+                              <div class="top2-s_half_box">
+                                   <p class="pt">1123 pt</p>
+                                   <img class="top_img" src="https://wac-cdn.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg?cdnVersion=231" alt="top img">
+                                   <h3>changyongg</h3>
                               </div>
                          </div>
-
-                         <div class="allranking-container">
-                              <!-- all ranking -->
-                              <div class="rank_tag">
-                                   <p class="no_tag">No</p>
-                                   <p class="user_tag">Users</p>
-                                   <p class="point_tag">Point</p>
+                         <div class="top_box top1_box">
+                              <div class="top1-s_half_box">
+                                   <p class="pt">1123 pt</p>
+                                   <img class="top_img" src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80" alt="top img">
+                                   <h3>changyongg</h3>
                               </div>
-
-                              <div class="long_box">
-                                   <div class="l_box">
-                                        <p class="rank_no">1</p>
-                                        <img class="rank_img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlIy4kRyqQYE-eyQQEMvkgyyDv0IEHECKuNQ&usqp=CAU" alt="top img">
-                                        <p class="rank_name">changyongg</p>
-                                        <p class="rank_pt">1123</p>
-                                        <button class="view_profile_btn">View Profile</button>
-                                   </div>
-                              </div>
-
-                              <div class="long_box">
-                                   <div class="l_box">
-                                        <p class="rank_no">2</p>
-                                        <img class="rank_img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlIy4kRyqQYE-eyQQEMvkgyyDv0IEHECKuNQ&usqp=CAU" alt="top img">
-                                        <p class="rank_name">changyongg</p>
-                                        <p class="rank_pt">1123</p>
-                                        <button class="view_profile_btn">View Profile</button>
-                                   </div>
-                              </div>
-
-                              <div class="long_box">
-                                   <div class="l_box">
-                                        <p class="rank_no">3</p>
-                                        <img class="rank_img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlIy4kRyqQYE-eyQQEMvkgyyDv0IEHECKuNQ&usqp=CAU" alt="top img">
-                                        <p class="rank_name">changyongg</p>
-                                        <p class="rank_pt">1123</p>
-                                        <button class="view_profile_btn">View Profile</button>
-                                   </div>
-                              </div>
-
-                              <div class="long_box">
-                                   <div class="l_box">
-                                        <p class="rank_no">4</p>
-                                        <img class="rank_img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlIy4kRyqQYE-eyQQEMvkgyyDv0IEHECKuNQ&usqp=CAU" alt="top img">
-                                        <p class="rank_name">changyongg</p>
-                                        <p class="rank_pt">1123</p>
-                                        <button class="view_profile_btn">View Profile</button>
-                                   </div>
+                         </div>
+                         <div class="top_box top3_box">
+                              <div class="top3-s_half_box">
+                                   <p class="pt">1123 pt</p>
+                                   <img class="top_img" src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80" alt="top img">
+                                   <h3>changyongg</h3>
                               </div>
                          </div>
                     </div>
                </div>
+               <div class="rank-tag_container">
+                    <div class="rank_tag">
+                         <p class="no_tag">No</p>
+                         <p class="user_tag">Users</p>
+                         <p class="point_tag">Point</p>
+                    </div>
+               </div>
+               <div class="all-ranking_dashboard">
+                    <div class="all-ranking_container">
+                         <div class="l_box">
+                              <div class="user_info">
+                                   <p class="rank_no">1</p>
+                                   <img class="rank_img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlIy4kRyqQYE-eyQQEMvkgyyDv0IEHECKuNQ&usqp=CAU" alt="top img">
+                                   <p class="rank_name">changyongg</p>
+                                   <p class="rank_pt">1123</p>
+                                   <button class="view_profile_btn">View Profile</button>
+                              </div>
+                         </div>
+                         <div class="l_box">
+                              <div class="user_info">
+                                   <p class="rank_no">1</p>
+                                   <img class="rank_img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlIy4kRyqQYE-eyQQEMvkgyyDv0IEHECKuNQ&usqp=CAU" alt="top img">
+                                   <p class="rank_name">changyongg</p>
+                                   <p class="rank_pt">1123</p>
+                                   <button class="view_profile_btn">View Profile</button>
+                              </div>
+                         </div>
+                         <div class="l_box">
+                              <div class="user_info">
+                                   <p class="rank_no">1</p>
+                                   <img class="rank_img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlIy4kRyqQYE-eyQQEMvkgyyDv0IEHECKuNQ&usqp=CAU" alt="top img">
+                                   <p class="rank_name">changyongg</p>
+                                   <p class="rank_pt">1123</p>
+                                   <button class="view_profile_btn">View Profile</button>
+                              </div>
+                         </div>
+                         <div class="l_box">
+                              <div class="user_info">
+                                   <p class="rank_no">1</p>
+                                   <img class="rank_img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlIy4kRyqQYE-eyQQEMvkgyyDv0IEHECKuNQ&usqp=CAU" alt="top img">
+                                   <p class="rank_name">changyongg</p>
+                                   <p class="rank_pt">1123</p>
+                                   <button class="view_profile_btn">View Profile</button>
+                              </div>
+                         </div>
+                    </div>
+               <div>
           </main>
 
      </div>
--- app/view/student/user.php
+++ app/view/student/user.php
@@ -22,91 +22,77 @@ $path = '../../../';
           <?php include '../layout/sidebar.php' ?>
 
           <main class="user--main main-content">
-<<<<<<< HEAD
                <h2 class="user__title">Users</h2>
                <!-- <p><?= $_SESSION['userId']; ?></p> -->
                
-               <div class="container">
-                    <div class="user-dashboard">
-                         <div class="user-container">
-                              <div class="long_box">
-                                   <div class="l_box">
-                                        <img class="user_img" src="https://wac-cdn.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg?cdnVersion=231" alt="top img">
-                                        <p class="username">changyongg</p>
-                                        <p class="user_id">U19320as932</p>
-                                        <div class="second_long_box">
-                                             <div class="second_l_box">
-                                                  <img class="user_img" src="https://wac-cdn.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg?cdnVersion=231" alt="top img">
-                                                  <p class="username">changyongg</p>
-                                                  <p class="user_id">U19320as932</p>
-                                             </div>
-                                        </div>
-                                   </div>
-                              </div>
-
-                              <div class="long_box">
-                                   <div class="l_box">
-                                        <img class="user_img" src="https://wac-cdn.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg?cdnVersion=231g" alt="top img">
-                                        <p class="username">changyongg</p>
-                                        <p class="user_id">U19320as932</p>
-                                        <div class="second_long_box">
-                                             <div class="second_l_box">
-                                                  <img class="user_img" src="https://wac-cdn.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg?cdnVersion=231" alt="top img">
-                                                  <p class="username">changyongg</p>
-                                                  <p class="user_id">U19320as932</p>
-                                             </div>
-                                        </div>
-                                   </div>
-                              </div>
-
-                              <div class="long_box">
-                                   <div class="l_box">
-                                        <img class="user_img" src="https://wac-cdn.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg?cdnVersion=231" alt="top img">
-                                        <p class="username">changyongg</p>
-                                        <p class="user_id">U19320as932</p>
-                                        <div class="second_long_box">
-                                             <div class="second_l_box">
-                                                  <img class="user_img" src="https://wac-cdn.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg?cdnVersion=231" alt="top img">
-                                                  <p class="username">changyongg</p>
-                                                  <p class="user_id">U19320as932</p>
-                                             </div>
-                                        </div>
-                                   </div>
-                              </div>
-
-                              <div class="long_box">
-                                   <div class="l_box">
-                                        <img class="user_img" src="https://wac-cdn.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg?cdnVersion=231" alt="top img">
-                                        <p class="username">changyongg</p>
-                                        <p class="user_id">U19320as932</p>
-                                        <div class="second_long_box">
-                                             <div class="second_l_box">
-                                                  <img class="user_img" src="https://wac-cdn.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg?cdnVersion=231" alt="top img">
-                                                  <p class="username">changyongg</p>
-                                                  <p class="user_id">U19320as932</p>
-                                             </div>
-                                        </div>
-                                   </div>
-                              </div>
-
-                              
-
-                              <div class="page_nav">
-                                   <button class="pre_nav_btn">Prev</button>
-                                   <p class="nav_num">2</p>
-                                   <button class="next_nav_btn">Next</button>
-                              </div>
-                              <div class="side_chat">
-                                   <button class="chat_btn">Chat&nbsp;&nbsp;<img class="chat_img" src="<?php echo $path; ?>public/img/chat.jpg" alt="chat"></button>
+               <div class="user_dashboard">
+                    <div class="user_container">
+                         <div class="user_box">
+                              <div class="user_info">
+                                   <img class="user_img" src="https://wac-cdn.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg?cdnVersion=231" alt="top img">
+                                   <p class="user_name">changyongg</p>
+                                   <p class="RP">RP: 213</p>
                               </div>
                          </div>
-                         
+                         <div class="user_box">
+                              <div class="user_info">
+                                   <img class="user_img" src="https://wac-cdn.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg?cdnVersion=231" alt="top img">
+                                   <p class="user_name">changyongg</p>
+                                   <p class="RP">RP: 213</p>
+                              </div>
+                         </div>
+                         <div class="user_box">
+                              <div class="user_info">
+                                   <img class="user_img" src="https://wac-cdn.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg?cdnVersion=231" alt="top img">
+                                   <p class="user_name">changyongg</p>
+                                   <p class="RP">RP: 213</p>
+                              </div>
+                         </div>
+                         <div class="user_box">
+                              <div class="user_info">
+                                   <img class="user_img" src="https://wac-cdn.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg?cdnVersion=231" alt="top img">
+                                   <p class="user_name">changyongg</p>
+                                   <p class="RP">RP: 213</p>
+                              </div>
+                         </div>
+                         <div class="user_box">
+                              <div class="user_info">
+                                   <img class="user_img" src="https://wac-cdn.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg?cdnVersion=231" alt="top img">
+                                   <p class="user_name">changyongg</p>
+                                   <p class="RP">RP: 213</p>
+                              </div>
+                         </div>
+                         <div class="user_box">
+                              <div class="user_info">
+                                   <img class="user_img" src="https://wac-cdn.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg?cdnVersion=231" alt="top img">
+                                   <p class="user_name">changyongg</p>
+                                   <p class="RP">RP: 213</p>
+                              </div>
+                         </div>
+                         <div class="user_box">
+                              <div class="user_info">
+                                   <img class="user_img" src="https://wac-cdn.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg?cdnVersion=231" alt="top img">
+                                   <p class="user_name">changyongg</p>
+                                   <p class="RP">RP: 213</p>
+                              </div>
+                         </div>
+                         <div class="user_box">
+                              <div class="user_info">
+                                   <img class="user_img" src="https://wac-cdn.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg?cdnVersion=231" alt="top img">
+                                   <p class="user_name">changyongg</p>
+                                   <p class="RP">RP: 213</p>
+                              </div>
+                         </div>
                     </div>
                     
                </div>
-=======
-               <h2 class="user__title main-title">Users</h2>
->>>>>>> 3ac939fe988ef772799295c7e43f17593624bf59
+               <div class="page-nav_container">
+                    <div class="page_nav">
+                         <button class="pre_nav_btn">Prev</button>
+                         <p class="nav_num">2</p>
+                         <button class="next_nav_btn">Next</button>
+                    </div>
+               </div>
           </main>
 
      </div>
